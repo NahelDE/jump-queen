1a3927f732f9ee8a2646c69af1648a088f209921
leo_leconte <leo.leconte@ens.univ-artois.fr>
2026-01-14 16:51:33 +0100

    normalement fonctionnel go tester après le pull

--- player.gd
+++ player.gd
@@ -2,19 +2,31 @@ extends CharacterBody2D
 
 @export var force_max = 1000.0
 @export var force_actuelle = 0.0
+@export var vitesse_marche = 200.0
 
 func _physics_process(delta):
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
+	# récup la direction
+	var direction = Input.get_axis("ui_left", "ui_right")
+		
 	if is_on_floor():
-		if Input.is_action_pressed("ui_accept"):
-			force_actuelle+=500*delta
-			force_actuelle = min(force_actuelle, force_max) # sinon le max est useless lol
-			
+		
+		if not Input.is_action_pressed("ui_accept"):
+			velocity.x = direction * vitesse_marche
+		else:
+			velocity.x = 0 # psq si on charge on s'arrête 
+			force_actuelle += 500 * delta
+			force_actuelle = min(force_actuelle, force_max)
+					
 		if Input.is_action_just_released("ui_accept"):
-			velocity.y=-force_actuelle # du coup le "-" car y monte en négatif 
-			force_actuelle=0
+			velocity.y = -force_actuelle
+			velocity.x = direction * (force_actuelle * 0.5) # test impulsion 
+			force_actuelle = 0
+			
+		if direction != 0:
+			$AnimatedSprite2D.flip_h = (direction < 0) #pour pas marcher à reculons
 
 	move_and_slide()
